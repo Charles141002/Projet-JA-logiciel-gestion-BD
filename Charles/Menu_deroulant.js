d1a4bab7403e2a2e52714bc093dfa1ef54e85e4a
Charles141002 <charlespelong@gmail.com>
2023-05-22 21:56:52 +0200

    Ajout de nouvelles fonctionnalités

--- Charles/Menu_deroulant.js
+++ Charles/Menu_deroulant.js
@@ -1,47 +1,9 @@
 function Menu(){
 
-    const menuButtons = document.querySelectorAll('.menu-button1');
-
-menuButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const submenu = button.nextElementSibling;
-    submenu.classList.toggle('active');
-  });
-});
-
-
 
     return (
 
-<ul class="menu1">
-  <li>
-    <a href="#" class="menu-button1">Menu 1</a>
-    <ul class="submenu1">
-      <li><a href="#">Option 1-1</a></li>
-      <li><a href="#">Option 1-2</a></li>
-      <li><a href="#">Option 1-3</a></li>
-    </ul>
-  </li>
-  <li>
-    <a href="#" class="menu-button1">Menu 2</a>
-    <ul class="submenu1">
-      <li><a href="#">Option 2-1</a></li>
-      <li><a href="#">Option 2-2</a></li>
-      <li><a href="#">Option 2-3</a></li>
-    </ul>
-  </li>
-  <li>
-    <a href="#" class="menu-button1">Menu 3</a>
-    <ul class="submenu1">
-      <li><a href="#">Option 3-1</a></li>
-      <li><a href="#">Option 3-2</a></li>
-      <li><a href="#">Option 3-3</a></li>
-    </ul>
-  </li>
-</ul>
-
-
-
+0
     )
 }
 
