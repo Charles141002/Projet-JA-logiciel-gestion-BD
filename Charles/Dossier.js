17c6ad219e9d5174ca8ec055761bee354a6d34c6
BaptisteBAYLE <baptiste.bayle1@outlook.com>
2023-05-24 13:49:57 +0200

    icones

--- Charles/Dossier.js
+++ Charles/Dossier.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import {useEffect } from 'react';
 import Reseau from "./Fiche_reseau";
 import Fiche from "./Fiche";
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon, library } from '@fortawesome/fontawesome-svg-core';
-import { faBuilding } from '@fortawesome/free-solid-svg-icons';
+import { faBuilding, faAngleLeft, faAngleDown, faUser } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBuilding);
+library.add(faBuilding, faAngleDown, faAngleLeft, faUser);
 
 
 function Dossier(props){
@@ -15,18 +15,19 @@ function Dossier(props){
     const [open, setOpen] = useState(false);
     const grouped = groupBy(props.datas[(isDossier ? 1 : 0)], isDossier ? "Reseau" : "Entreprise");
     const { count, setCount } = props.customHook;
-
+    let icone = (!(isDossier) && < FontAwesomeIcon icon="building"/>)
 
     console.log('groupe ', grouped[props.nom]);
         return (
             <div className={(isDossier ? "" : "sous-") + "dossier"} >
                 <div class="bloc-titre-deroulant" onDoubleClick={() => setOpen(!(open))} onClick={() => setCount(3)}>
-                    <h1>{props.nom}</h1>
-                    <h1 onClick={() => setOpen(!(open))}>{(open ? "^": "<" )}</h1>
+                    
+                    <h1> {icone} {props.nom}</h1>
+                    <h1 onClick={() => setOpen(!(open))}>{(open ? <FontAwesomeIcon icon="angle-down"/>: <FontAwesomeIcon icon="angle-left"/> )}</h1>
                 </div>
                 {(open && (!isDossier) && grouped[props.nom]) && grouped[props.nom].map(elt => 
                     <div className="bloc-titre-deroulant">
-                        <p>{elt.Nom} {elt.Prenom}</p>
+                        <p> <FontAwesomeIcon icon="user"/> {elt.Nom} {elt.Prenom}</p>
                     </div>
                 )}
                 {(open && isDossier && grouped[props.nom]) && grouped[props.nom].map(elt => <Dossier nom={elt.Nom} datas={props.datas} class="entreprise" customHook={props.customHook}/>)}
